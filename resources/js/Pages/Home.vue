7da63a266eda8f0dbd2a5489f2a896cdfc701ee3
kazadesky <kazadesky123@gmail.com>
2024-10-04 17:04:30 +0700

    learn-4/configuration default global or personal Head component statement and Link component statement

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,4 +1,6 @@
 <script setup>
+// personal use component in this page only
+// import { Head } from '@inertiajs/vue3';
 import Demo from '../Layout/Demo.vue';
 
 // default layout in this page
@@ -6,5 +8,12 @@ defineOptions({ layout: Demo });
 </script>
 
 <template>
+
+    <Head>
+        <!-- this is personal title page -->
+        <!-- and this standart include title in personal page -->
+        <title>Home Page</title>
+        <meta head-key="description" name="description" content="This is the home page meta.">
+    </Head>
     <h1>Home Page</h1>
 </template>
