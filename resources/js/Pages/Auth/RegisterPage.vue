9cf135b14cf1c0fd0a9b036716cd6186e212f85f
kazadesky <kazadesky123@gmail.com>
2024-10-04 20:13:23 +0700

    learn-11/create component Text Input for simple component for page use here

--- resources/js/Pages/Auth/RegisterPage.vue
+++ resources/js/Pages/Auth/RegisterPage.vue
@@ -6,6 +6,7 @@
 
 // use helper form from inertia for validation
 import { useForm } from '@inertiajs/vue3';
+import TextInput from '../Components/TextInput.vue';
 
 const form = useForm({
     name: null,
@@ -28,39 +29,19 @@ const submit = () => {
     <div class="w-full flex flex-col space-y-5 justify-center items-center mt-5">
         <h1 class="text-2xl font-bold uppercase">Register Page</h1>
         <form @submit.prevent="submit" class="w-full max-w-md p-5 rounded-md shadow-lg bg-white border-2">
-            <div class="mb-3">
-                <label for="name" class="font-medium block mb-2">Name</label>
-                <input type="text" v-model="form.name" id="name"
-                    class="outline-none rounded-md border-2 border-gray-200 px-3 h-12 w-full transition duration-300 focus:border-gray-300"
-                    placeholder="Full name" />
-                <small class="text-red-600" v-if="form.errors.name">{{ form.errors.name }}</small>
-            </div>
-            <div class="mb-3">
-                <label for="email" class="font-medium block mb-2">Email</label>
-                <input type="email" v-model="form.email" id="email"
-                    class="outline-none rounded-md border-2 border-gray-200 px-3 h-12 w-full transition duration-300 focus:border-gray-300"
-                    placeholder="email@example.com" />
-                <small class="text-red-600" v-if="form.errors.email">{{ form.errors.email }}</small>
-            </div>
-            <div class="mb-3">
-                <label for="password" class="font-medium block mb-2">Password</label>
-                <input type="password" v-model="form.password" id="password"
-                    class="outline-none rounded-md border-2 border-gray-200 px-3 h-12 w-full transition duration-300 focus:border-gray-300"
-                    placeholder="******" />
-                <small class="text-red-600" v-if="form.errors.password">{{ form.errors.password }}</small>
-            </div>
-            <div class="mb-3">
-                <label for="password_confirmation" class="font-medium block mb-2">Confirm Password</label>
-                <input type="password" v-model="form.password_confirmation" id="password_confirmation"
-                    class="outline-none rounded-md border-2 border-gray-200 px-3 h-12 w-full transition duration-300 focus:border-gray-300"
-                    placeholder="******" />
-                <small class="text-red-600" v-if="form.errors.password_confirmation">{{
-                    form.errors.password_confirmation }}</small>
-            </div>
+            <TextInput forLabel="name" nameLabel="Name" typeInput="text" v-model="form.name" idInput="name"
+                placeInput="Full name" :message="form.errors.name" />
+            <TextInput forLabel="email" nameLabel="Email" typeInput="email" v-model="form.email" idInput="email"
+                placeInput="email@example.com" :message="form.errors.email" />
+            <TextInput forLabel="password" nameLabel="Password" typeInput="password" v-model="form.password"
+                idInput="password" placeInput="******" :message="form.errors.password" />
+            <TextInput forLabel="password_confirmation" nameLabel="Confirm Password" typeInput="password"
+                v-model="form.password_confirmation" idInput="password_confirmation" placeInput="******"
+                :message="form.errors.password_confirmation" />
             <div class="w-full flex flex-col space-y-3 items-center">
                 <button type="submit"
-                    class="w-full outline-none bg-blue-600 flex items-center justify-center text-white uppercase font-medium transition duration-300 hover:bg-blue-700 focus:bg-blue-700 rounded-md shadow-sm h-11"
-                    :disabled="form.processing">Register</button>
+                    class="w-full outline-none bg-blue-600 flex items-center justify-center text-white uppercase font-medium transition duration-300 hover:bg-blue-700 focus:bg-blue-700 rounded-md shadow-sm h-11 disabled:bg-gray-500"
+                    :disabled="form.processing">{{ form.processing ? 'Loading' : 'Register' }}</button>
                 <p class="text-gray-600">Already have account?
                     <Link href="#"
                         class="text-blue-600 outline-none hover:underline hover:underline-offset-2 focus:underline focus:underline-offset-2">
---
+++ resources/js/Pages/Components/TextInput.vue
@@ -0,0 +1,37 @@
+<script setup>
+const model = defineModel({
+    type: null,
+    required: true,
+});
+
+defineProps({
+    nameLabel: {
+        type: String,
+        required: true,
+    },
+    forLabel: {
+        type: String,
+        required: true,
+    },
+    typeInput: {
+        type: String,
+        default: 'text',
+    },
+    idInput: {
+        type: String,
+        required: true,
+    },
+    placeInput: String,
+    message: String,
+});
+</script>
+
+<template>
+    <div class="mb-3">
+        <label :for="forLabel" class="font-medium block mb-2">{{ nameLabel }}</label>
+        <input :type="typeInput" v-model="model" :id="idInput"
+            class="outline-none rounded-md border-2 border-gray-300 px-3 h-12 w-full transition duration-300 focus:border-blue-600"
+            :class="{ '!border-red-600': message }" :placeholder="placeInput" v-bind="$attrs" />
+        <small class="text-red-600" v-if="message">{{ message }}</small>
+    </div>
+</template>
